29ad3d62bb973e9b0066680d4ffa85449ffe179e
Iacchus Mercurius <kassivs.@gmail.com>
2020-07-17 18:19:14 -0300

    ok

--- test.py
+++ test.py
@@ -15,22 +15,5 @@ with open(FILENAME, "r") as fd:
 preproc = preprocessor.ITMLPreprocessor(text=lines)
 proc = processor.ITMLProcessor(preproc)
 
-#pprint.pprint(preproc.groups)
-#pprint.pprint(preproc.parsed)
-#print(preproc.parsed)
-#print(*[item for item in preproc.parsed_list], sep='\n')
-#pprint.pprint(preproc.parsed_list)
-# print(preproc.groups)
-#print(preproc.parsed_list)
-#print(preproc.item_list)
-# pprint.pprint("\join"*[item for item in preproc.item_list])
-# for item in preproc.item_list:
-#print(proc.item_list)
-#print(proc.parsed_list)
 for item in proc.parsed_list:
-#for item in proc.item_list:
     pprint.pprint(item, width=160)
-    #print(repr(item))
-    #print(*[el for el in item], sep="\n‧‧‧‧")
-    #print(*[el for el in item], sep="\n‧‧‧‧")
-    #print(item)
